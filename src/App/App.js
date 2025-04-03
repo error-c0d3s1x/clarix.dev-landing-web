9c4f58cde6c0c73050e1b57903e302e587b6a35d
error-c0d3s1x <error.c0d3s1x@gmail.com>
2025-04-04 01:36:38 +0800

    refactor: try separating each per feature?

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import BlackholeAnimation from './BlackholeAnimation';
+import BlackholeAnimation from '../Blackhole/BlackholeAnimation';
 import './App.css';
 
 function App() {
